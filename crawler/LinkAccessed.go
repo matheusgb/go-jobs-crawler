863aa4e91f9c867e4d13eadc6c24eb0548c6374e
matheusgb <matheusgbr98@gmail.com>
2023-09-05 19:52:32 -0300

    feat: create linkedin page access

---
+++ crawler/LinkAccessed.go
@@ -0,0 +1,6 @@
+package crawler
+
+import "golang.org/x/net/html"
+
+func LinkAccessed(document *html.Node) {
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/matheusgb/go-jobs-crawler/crawler"
+)
+
+func main() {
+	crawler.VisitPage()
+}
